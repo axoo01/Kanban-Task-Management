6ce93854d08619100da07afc98d4a57629dad5e1
axoo01 <juniorax03@gmail.com>
2026-05-07 17:36:24 +0200

    refactor: refined final touch merge

--- src/app/components/layout-shell/layout-shell.component.ts
+++ src/app/components/layout-shell/layout-shell.component.ts
@@ -1,11 +1,13 @@
-import { Component, signal, inject, computed } from '@angular/core';
+import { Component, signal, inject, computed, HostListener } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { ThemeService } from '../../services/theme.service';
 import { BoardService } from '../../services/board.service';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-layout-shell',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './layout-shell.component.html',
   styleUrl: './layout-shell.component.scss'
 })
@@ -14,13 +16,28 @@ export class LayoutShellComponent {
   isDarkMode = this.themeService.darkMode;
   boardService = inject(BoardService);
 
+  isMobileView = signal(window.innerWidth <= 768);
+  isMobileSidebarOpen = signal(false);
+  isSidebarHidden = signal(false);
+
   headerTitle = computed(() => 
     this.boardService.currentBoard()?.name || 'Platform Launch'
   );
 
-  boards = this.boardService.boards;
+  
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    const isMobile = event.target.innerWidth <= 768;
+    this.isMobileView.set(isMobile);
+   
+    if (!isMobile) this.isMobileSidebarOpen.set(false);
+  }
 
-  isSidebarHidden = signal(false);
+  toggleMobileSidebar() {
+    if (this.isMobileView()) {
+      this.isMobileSidebarOpen.update(val => !val);
+    }
+  }
 
   toggleSidebar() {
     this.isSidebarHidden.update(val => !val);
@@ -29,5 +46,4 @@ export class LayoutShellComponent {
   toggleTheme() {
     this.themeService.toggleTheme();
   }
-
 }
